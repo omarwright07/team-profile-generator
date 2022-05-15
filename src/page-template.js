e6b1aed3338c3ec1358119e13d6bc8a658eb0657
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-05-15 00:37:33 -0500

    filter and sort employee in order of manager>engineer>intern

--- src/page-template.js
+++ src/page-template.js
@@ -2,24 +2,28 @@ const generateCards = function (employees) {
     var cards = [];
     employees.forEach(employee => {
         var roleInfo = ``;
+        var roleIcon = "";
         // Manager        
         if (employee.role == 'Manager') {
             roleInfo = `<li><p>Office Number: ${employee.officeNumber}</p></li>`;
+            roleIcon = "oi oi-clipboard";
         }
         // Engineer
         if (employee.role == 'Engineer') {
-            roleInfo = `<li class="d-flex"><p>GitHub:</p><a href="https://github.com/${employee.gitHub}">${employee.gitHub}</a></li>`;
+            roleInfo = `<li class="d-flex"><p>GitHub:</p><a href="https://github.com/${employee.gitHub}" target="_blank">${employee.gitHub}</a></li>`;
+            roleIcon = "oi oi-monitor";
         }
         // Intern        
         if (employee.role == 'Intern') {
             roleInfo = `<li><p>School: ${employee.school}</p></li>`;
+            roleIcon = "oi oi-book";
         }
 
         let card = `
         <div class="card shadow">
             <div class="card-header">
                 <h1>${employee.name}</h1>                              
-                <h2><span class="oi oi-book"></span>${employee.role}</h2>
+                <h2><span class="${roleIcon}"></span>${employee.role}</h2>
             </div>
             <div class="card-body align-middle">
                 <ul>
@@ -41,6 +45,12 @@ const generateCards = function (employees) {
 };
 
 const generateHTML = function (employeesData) {
+    const manager = employeesData.filter(employeesData => employeesData.role === 'Manager');
+    // console.log(manager);
+    const engineers = employeesData.filter(employeesData => employeesData.role === 'Engineer');
+    // console.log(engineers);
+    const interns = employeesData.filter(employeesData => employeesData.role === 'Intern');
+    // console.log(interns);
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -48,30 +58,28 @@ const generateHTML = function (employeesData) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
         <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         />
         <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-          crossorigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
-          rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
+      rel="stylesheet"
         />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" />
-        <link rel="stylesheet" href="style.css">
-        <title>My Team</title>
+    <link rel="stylesheet" href="style.css">
+        
+    <title>My Team</title>
     </head>
     <body>
         <header class="shadow">
             <h1 class="">My Team</h1>
         </header>
         <main class="d-flex flex-wrap justify-content-center">
-        ${generateCards(employeesData)}
+        ${generateCards(manager)}
+        ${generateCards(engineers)}
+        ${generateCards(interns)}
         </main>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
     </body>
